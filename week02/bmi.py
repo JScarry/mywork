8f8fab11286575848a1c4dfa67930bda8f11e837
G00411365 <G00411365@gmit.ie>
2022-01-29 21:40:47 +0000

    bmi in week02

--- week02/bmi.py
+++ week02/bmi.py
@@ -1,3 +1,25 @@
 #Body mass index calculator
 #Author Jarlath Scarry
 
+name = input ('Enter your name:')
+print ('Hello {}\n Please answer the following questions and we\
+    \n will calculate your Body Mass Index (BMI)\n'.format(name))
+
+age = input ('What is your age?:')
+weight = float(input ('What is your weight(KG)?:'))
+height = float(input ('What is your height(CM)?:'))
+
+print ('Your age is {} , weight {}KG and height is {}CM.\n'.format(age, weight, height))
+
+#ans = input ('Are these details correct? y/n\n')
+#print (ans)
+
+bmi = weight / (height/100)**2
+
+print ('{} your BMI is {:.2f}' .format(name,bmi))
+
+
+
+
+
+
---
+++ week02/tests.py
@@ -0,0 +1,11 @@
+
+
+price = 49
+txt = "The price is {:.2f} dollars"
+print(txt.format(price))
+
+quantity = 3
+itemno = 567
+price = 49
+myorder = "I want {0} pieces of item number {1} for {2:.2f} dollars."
+print(myorder.format(quantity, itemno, price))
